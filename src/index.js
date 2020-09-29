490cffb6e6b17821ec104838436dd8d17ae57909
Ivan Gawr <ivangawr1993>
2020-09-29 21:59:27 +0300

    fix: implement task is true

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort (matrix){
+  if(matrix){
+    matrix.filter((el, i)=> i % 2 == 1 ? el.reverse() : 0);
+     return matrix.reduce((acc, elar) => acc.concat(elar), []);
+    }
+    return [];
+  };
+  
